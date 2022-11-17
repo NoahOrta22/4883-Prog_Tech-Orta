4da77f15122a05195f7bea7a3514de8192791af6
Noah Orta <50061473+NoahOrta22@users.noreply.github.com>
2022-11-17 14:35:04 -0600

    Create main.cpp

---
+++ Assignments/A04/Hashmat/main.cpp
@@ -0,0 +1,19 @@
+/**
+* Noah Orta
+* 4883 Programming Techniques
+* 8/30/2022
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long B = 0, A = 0;
+  
+  while (cin >> A >> B) {
+    
+    cout << abs(A - B) << '\n';
+    
+  }
+  return 0;
+}
